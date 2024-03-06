8e8b4982f4f1a60df9b4cfd590f099b153b2da98
Will Cooley <william.cooley@gmail.com>
2024-03-05 21:23:33 -0500

    added routes

---
+++ client/src/components/App.jsx
@@ -0,0 +1,15 @@
+import {Container, Typography} from '@mui/material'
+
+
+function App() {
+
+  return (
+    <Container>
+      <Typography>
+        Lesson Studio
+      </Typography>
+    </Container>
+  )
+}
+
+export default App

d2faa8a074446ead8854ce030038339f8442e04c
Will Cooley <william.cooley@gmail.com>
2024-03-05 21:32:36 -0500

    added Header and NavBar Components

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -1,10 +1,12 @@
 import {Container, Typography} from '@mui/material'
+import Header from './Header'
 
 
 function App() {
 
   return (
     <Container>
+      <Header />
       <Typography>
         Lesson Studio
       </Typography>
---
+++ client/src/components/NavBar.jsx
@@ -0,0 +1,14 @@
+import {NavLink} from 'react-router-dom';
+import {Container, Typography} from '@mui/material';
+
+import React from 'react';
+
+function NavBar() {
+    return (
+        <Container>
+            <Typography>NavBar</Typography>
+        </Container>
+    );
+};
+
+export default NavBar;

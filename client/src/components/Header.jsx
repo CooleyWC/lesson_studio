48a4e9cae2a802abff187f5ffb549140d09e0aab
Will Cooley <william.cooley@gmail.coom>
2024-03-05 21:32:36 -0500

    added Header and NavBar Components

---
+++ client/src/components/Header.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import NavBar from './NavBar';
+import {Container, Typography} from '@mui/material'
+
+function Header(props) {
+    return (
+        <Container>
+            <NavBar />
+        </Container>
+    );
+}
+
+export default Header;

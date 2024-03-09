43aceffb726067f934a9cbc86e32e513ca12f8be
Will Cooley <william.cooley@gmail.coom>
2024-03-09 10:40:56 -0500

    added instrctor data to App.jsx

--- server/models.py
+++ server/models.py
@@ -4,6 +4,16 @@ from sqlalchemy.ext.associationproxy import association_proxy
 from config import db
 
 
+# class Student(db.Model, SerializerMixin):
+#     __tablename__ = 'students'
+
+#     id=db.Column(db.Integer, primary_key=True)
+#     name=db.Column(db.String, unique=True, nullable=False)
+#     email=db.Column(db.String, unique=True, nullable=False)
+#     age=db.Column(db.Integer, db.CheckConstraint('age < 3'), nullable=False)
+#     primary_instrument=db.Columns(db.String, nullable=False)
+
+
 class Instructor(db.Model, SerializerMixin):
     __tablename__ = 'instructors'
 
@@ -14,3 +24,9 @@ class Instructor(db.Model, SerializerMixin):
     experience=db.Column(db.Integer, nullable=False, default=0)
     instrument=db.Column(db.String, nullable=False)
     photo=db.Column(db.String)
+
+# class Lesson(db.Model, SerializerMixin):
+#     __tablename__ = 'lessons'
+
+#     id=db.Column(db.Integer, primary_key=True)
+#     date=db.Column()

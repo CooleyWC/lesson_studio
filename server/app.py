dfc9e568b7d5b48edb3d2f2076cefa80a9c14776
Will Cooley <william.cooley@gmail.com>
2024-03-10 22:24:04 -0400

    added User class and Signup route

--- server/app.py
+++ server/app.py
@@ -1,8 +1,44 @@
-from config import app, api
+from config import app, api, db
 from flask_restful import Resource
+from flask import request, session
 
-from models import Instructor
+from models import Instructor, User
 
+
+class Signup(Resource):
+   def post(self):
+      json = request.get_json()
+
+      user=User(
+         username=json.get('username'),
+         email=json.get('email'),
+         age=json.get('age'),
+         primary_instrument=json.get('primary_instrument')
+      )
+
+      user.password_hash = json['password']
+
+      try:
+         db.session.add(user)
+         db.session.commit()
+
+         session['user_id'] = user.id
+
+         response_data = {
+            'id': user.id,
+            'username': user.username,
+            'email': user.email,
+            'age': user.age,
+            'primary_instrument': user.primary_instrument
+         }
+
+         return response_data, 201
+      
+      except:
+         error={'error': 'invalid input'}
+         return error, 422
+      
+api.add_resource(Signup, '/signup', endpoint='signup')
 
 class Instructors(Resource):
     def get(self):
--- server/models.py
+++ server/models.py
@@ -1,19 +1,41 @@
 from sqlalchemy_serializer import SerializerMixin
 from sqlalchemy.ext.associationproxy import association_proxy
+from sqlalchemy.ext.hybrid import hybrid_property
 
 from config import db, bcrypt
 
 
-# class Student(db.Model, SerializerMixin):
-#     __tablename__ = 'students'
+class User(db.Model, SerializerMixin):
+    __tablename__ = 'user'
 
-#     id=db.Column(db.Integer, primary_key=True)
-#     name=db.Column(db.String, unique=True, nullable=False)
-#     email=db.Column(db.String, unique=True, nullable=False)
-#     age=db.Column(db.Integer, db.CheckConstraint('age < 3'), nullable=False)
-#     primary_instrument=db.Columns(db.String, nullable=False)
+    id=db.Column(db.Integer, primary_key=True)
+    username=db.Column(db.String, unique=True, nullable=False)
+    email=db.Column(db.String, unique=True, nullable=False)
+    age=db.Column(db.Integer, db.CheckConstraint('age < 3 name=age_check'), nullable=False)
+    age=db.Column(db.Integer, nullable=False)
+    primary_instrument=db.Column(db.String, nullable=False)
 
-#     _password_hash = 
+    _password_hash = db.Column(db.String, nullable=False)
+
+    # __table_args__ = (
+    #     db.CheckConstraint('(age <3) name=age_check'),
+    # )
+
+    @hybrid_property
+    def password_hash(self):
+        raise AttributeError('password is not viewable')
+    
+    @password_hash.setter
+    def password_hash(self, password):
+        password_hash=bcrypt.generate_password_hash(
+            password.encode('utf-8')
+        )
+        self._password_hash = password_hash.decode('utf-8')
+
+    def authenticate(self, password):
+        return bcrypt.check_password_hash(
+            self._password_hash, password.encode('utf-8')
+        )
 
 
 class Instructor(db.Model, SerializerMixin):

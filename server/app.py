1ba54aafda8139dec24c956edb489e733d62cdbb
Will Cooley <william.cooley@gmail.coom>
2024-03-12 23:06:03 -0400

    added serilize rules and relationships and association proxys to models

--- server/app.py
+++ server/app.py
@@ -2,7 +2,7 @@ from config import app, api, db
 from flask_restful import Resource
 from flask import request, session
 
-from models import Instructor, User
+from models import Instructor, User, Lesson
 
 
 class Signup(Resource):
@@ -38,14 +38,31 @@ class Signup(Resource):
          error={'error': 'invalid input'}
          return error, 422
       
-api.add_resource(Signup, '/signup', endpoint='signup')
+api.add_resource(Signup, '/api/signup', endpoint='signup')
 
 class Instructors(Resource):
     def get(self):
         instructors = [instructor.to_dict() for instructor in Instructor.query.all()]
         return instructors, 200
 
-api.add_resource(Instructors, '/instructors')
+api.add_resource(Instructors, '/api/instructors')
+
+class Users(Resource):
+   def get(self):
+      users = [user.to_dict() for user in User.query.all()]
+      return users, 200
+
+api.add_resource(Users, '/api/users')
+
+class Lessons(Resource):
+   def get(self):
+      lessons = [lesson.to_dict() for lesson in Lesson.query.all()]
+      return lessons, 200
+   
+api.add_resource(Lessons, '/api/lessons')
+
+
+
 
 if __name__ == "__main__":
   app.run(port=5555, debug=True)

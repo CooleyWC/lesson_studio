d7c3899f64175ac16fa6a3cd1e4033963605837a
Will Cooley <william.cooley@gmail.coom>
2024-03-13 23:24:57 -0400

    added login and checksession routes and updated seed file

--- server/app.py
+++ server/app.py
@@ -40,6 +40,42 @@ class Signup(Resource):
       
 api.add_resource(Signup, '/api/signup', endpoint='signup')
 
+class CheckSession(Resource):
+   def get(self):
+      user_id=session['user_id']
+      try:
+         user=User.query.filter(User.id==user_id).first()
+         return user.to_dict(), 200
+      except:
+         error = {'error': 'invalid email or password'}
+         return error, 401
+      
+api.add_resource(CheckSession, '/api/check_session', endpoint='check_session')
+
+class Login(Resource):
+   def post(self):
+
+      # json=request.get_json()
+      # user=User.query.filter(User.email==json['email']).first()
+      # if user:
+      #    if user.authenticate(json.get('password')):
+      #       session['user_id']=user.id
+      #       return user.to_dict(), 200
+      #    else:
+      #       return {'error': 'invalid'}
+      try:
+         email=request.get_json()['email']
+         user=User.query.filter(User.email==email).first()
+         password=request.get_json()['password']
+         if user.authenticate(password):
+            session['user_id'] = user.id
+            return user.to_dict(), 200
+      except:
+         error = {'error': 'invalid email or password'}
+         return error, 401
+      
+api.add_resource(Login, '/api/login', endpoint='login')
+
 class Instructors(Resource):
     def get(self):
         instructors = [instructor.to_dict() for instructor in Instructor.query.all()]

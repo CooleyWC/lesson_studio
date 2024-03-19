08e3cb21df0605e74f7dbf0bb2d537851a72aa53
Will Cooley <william.cooley@gmail.com>
2024-03-19 15:48:19 -0400

    added conditional logic to Dashboard

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -2,8 +2,30 @@ import {Container, Typography} from '@mui/material'
 import Header from './Header';
 import {Outlet} from 'react-router-dom';
 import { useEffect, useState} from 'react';
+import { useAuth } from './context/AuthProvider';
 
 function App() {
+
+  const {user, login, logout} = useAuth()
+
+
+  useEffect(()=>{
+    checkUser()
+  }, [])
+
+  const checkUser = () => {
+    fetch('/api/check_session')
+    .then(res=>{
+      if(res.ok){
+        console.log('youre logged in')
+        login()
+      } else {
+        console.log('your not logged in', res.error)
+        logout()
+      }
+    })
+  }
+  
 
   return (
     <Container>
--- client/src/components/pages/Dashboard.jsx
+++ client/src/components/pages/Dashboard.jsx
@@ -1,11 +1,29 @@
 import React from 'react';
-import { Typography } from '@mui/material';
+import { Typography, Box } from '@mui/material';
+import { useAuth } from '../context/AuthProvider';
 
 function Dashboard() {
+
+    const {user} = useAuth()
+
+    if(user === null) return <Typography sx={{color: 'white', fontSize: '50px', paddingTop: '100px'}}>Loading....</Typography>
+
+    if(!user) {
+    
+        return (
+            <Typography sx={{color: 'white', fontSize: '50px', paddingTop: '100px'}}>Hi! Please login to view dashboard</Typography>
+        )
+        }
+
     return (
-        <div>
-            <Typography sx={{fontSize: '100px'}}>Dashboard</Typography>
-        </div>
+        <>
+        <Box sx={{paddingTop: '50px', backgroundColor: '#E0E1DD', width: '100%'}}>
+            <Typography sx={{fontSize: '80px', display: 'flex', justifyContent: 'center', alignItems: 'center'}}>Dashboard</Typography>
+        </Box>
+        <Box>
+            <Typography>Your profile</Typography>
+        </Box>
+        </>
     );
 }
 

37f8ee167a8e7786c0d10433dfcdb6d05e156c7e
Will Cooley <william.cooley@gmail.com>
2024-03-24 22:05:41 -0400

    added search instructor feature to filter

---
+++ client/src/components/cards/FilterCard.jsx
@@ -0,0 +1,55 @@
+import React from 'react';
+import {Box, InputLabel, Select, Paper, TextField, MenuItem, FormControl} from '@mui/material';
+
+function FilterCard({handleNameSearch, nameSearch, handleInstrumentSearch, instrumentSearch}) {
+
+    const VALID_INSTRUMENTS = [
+        'all',
+        'piano', 
+        'drums', 
+        'bass', 
+        'guitar', 
+        'trumpet',
+        'trombone', 
+        'tuba', 
+        'french horn', 
+        'cello', 
+        'violin', 
+        'viola', 
+        'voice'
+    ]
+
+
+    return (
+        <Paper sx={{height: '80px', paddingTop: '15px', paddingBottom: '15px', width: '100%', display: 'flex', justifyContent: 'space-around'}}>
+            <Box sx={{display: 'flex', flexDirection: 'row', alignItems: 'center'}}>
+                <FormControl>
+                    <TextField variant='outlined' sx={{paddingRight: '40px'}}label='Search by Name' onChange={handleNameSearch} value={nameSearch}/>
+                </FormControl>
+                <FormControl>
+                <InputLabel>Search By Instrument</InputLabel>
+                <Select 
+                    label='Search By Instrument'
+                    onChange={handleInstrumentSearch} 
+                    value={instrumentSearch} 
+                    multiple
+                    sx={{width: '200px'}}
+                    >
+                    {VALID_INSTRUMENTS.map((instrument)=>{
+                        return(
+                            <MenuItem
+                                key={instrument}
+                                value={instrument}
+                            >
+                                {instrument}
+                            </MenuItem>
+                        )
+                    })}
+                </Select>
+                </FormControl>
+            </Box>
+        </Paper>
+    );
+}
+
+export default FilterCard;
--- client/src/components/pages/Faculty.jsx
+++ client/src/components/pages/Faculty.jsx
@@ -2,9 +2,12 @@ import React from 'react';
 import { Box, Typography, Grid } from '@mui/material';
 import { useEffect, useState } from 'react';
 import FacultyCard from '../cards/FacultyCard';
+import FilterCard from '../cards/FilterCard';
 
 function Faculty() {
     const [instructors, setInstructors] = useState([])
+    const [nameSearch, setNameSearch] = useState('')
+    const [instrumentSearch, setInstrumentSearch] = useState([])
 
 
     useEffect(()=>{
@@ -17,11 +20,38 @@ function Faculty() {
         .catch(error=>console.log(error))
     }, [])
 
-    console.log(instructors)
+    // console.log(instructors)
+
+    const handleNameSearch = (e) =>{
+        console.log(e.target.value)
+        setNameSearch(e.target.value)
+    }
+
+
+    const filteredNames = instructors.filter((instructor)=>{
+        
+        if(nameSearch===''){
+            return true
+        }
+    
+        if(instructor.name.toLowerCase().includes(nameSearch.toLowerCase())){
+            return true
+        } 
+    })
+
+    console.log(nameSearch)
+    console.log(filteredNames)
+    
+    const handleInstrumentSearch = (e) =>{
+        console.log(e.target.value)
+        setInstrumentSearch(e.target.value)
+    }
+
+
 
     const facultyCards = 
         <Grid container spacing={5}>
-            {instructors.map((instructor)=>(
+            {filteredNames.map((instructor)=>(
                 <Grid item key={instructor.id} xs={12} sm={6} md={3} lg={3}>
                     <FacultyCard
                         key={instructor.id}
@@ -41,6 +71,14 @@ function Faculty() {
                 <Typography variant='h2' sx={{color: 'black', fontSize: '80px', fontWeight: 'light', display: 'flex', justifyContent: 'center', alignItems: 'center'}}>
                     Faculty
                 </Typography>
+            </Box>
+            <Box>
+                <FilterCard 
+                    handleNameSearch={handleNameSearch}
+                    nameSearch={nameSearch}
+                    handleInstrumentSearch={handleInstrumentSearch}
+                    instrumentSearch={instrumentSearch}
+                />
             </Box>
             <Box sx={{flexGrow: 1}}>
                 {facultyCards}

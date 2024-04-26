82288a38d8b70ddbd4345f469b4feba721ff32a6
Will Cooley <william.cooley@gmail.com>
2024-04-26 12:18:55 -0400

    cleaned up AuthProvider

--- client/src/components/context/AuthProvider.jsx
+++ client/src/components/context/AuthProvider.jsx
@@ -1,14 +1,11 @@
 import React, { createContext, useContext, useState } from 'react';
 
-// create context for user data
 const AuthContext = createContext({})
 
-// useAuth hook
 export const useAuth = ()=>{
     return useContext(AuthContext)
 }
 
-// create a provider component
 export const AuthProvider = ({children}) => {
 
     const [user, setUser] = useState(null)

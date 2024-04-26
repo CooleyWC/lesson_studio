ca79b4c16f6afa0f895615cae1b53004edfebd93
Will Cooley <william.cooley@gmail.com>
2024-04-26 12:23:01 -0400

    cleaned up filter

--- client/src/components/forms/Filter.jsx
+++ client/src/components/forms/Filter.jsx
@@ -19,7 +19,6 @@ function Filter({handleNameSearch, nameSearch, handleInstrumentSearch, instrumen
         'voice'
     ]
 
-
     return (
         <Paper sx={{height: '80px', paddingTop: '15px', paddingBottom: '15px', width: '100%', display: 'flex', justifyContent: 'space-around'}}>
             <Box sx={{display: 'flex', flexDirection: 'row', alignItems: 'center'}}>
@@ -32,7 +31,6 @@ function Filter({handleNameSearch, nameSearch, handleInstrumentSearch, instrumen
                     label='Search By Instrument'
                     onChange={handleInstrumentSearch} 
                     value={instrumentSearch} 
-                    // multiple
                     sx={{width: '200px'}}
                     >
                     {VALID_INSTRUMENTS.map((instrument)=>{

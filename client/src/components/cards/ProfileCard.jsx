9ac690aad41d9198ab85b5397f7487e3b14f0cee
Will Cooley <william.cooley@gmail.coom>
2024-04-29 12:08:47 -0400

    cleaned up ProfileCard

--- client/src/components/cards/ProfileCard.jsx
+++ client/src/components/cards/ProfileCard.jsx
@@ -1,11 +1,11 @@
 import React from 'react';
-import {Card, CardContent, CardHeader, Typography} from '@mui/material'
+import {Card, CardContent, Typography} from '@mui/material'
 
 function ProfileCard({name, age, primary_instrument}) {
     return (
         <Card>
             <CardContent sx={{paddingLeft: '10px'}}>
-            <Typography>Name: {name}</Typography>
+            <Typography>Name: {name.charAt(0).toUpperCase() + name.slice(1)}</Typography>
             <Typography>Age: {age}</Typography>
             <Typography>Primary Instrument: {primary_instrument.charAt(0).toUpperCase() + primary_instrument.slice(1)}</Typography>
             </CardContent>
